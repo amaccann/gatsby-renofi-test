ec9aef2cd4447589535c0b83ee5e49f27ba0cd4b
Andrew <andrew.maccann@gmail.com>
2024-05-14 08:37:02 +0100

    Testing SSR

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,29 @@
-import * as React from "react"
-import {Box, Text} from 'rebass';
+import * as React from 'react'
+import {Box, Image, Text} from 'rebass';
 
 import useSiteMetadata from '../hooks/useSiteMetadata'
 import {Router} from '@reach/router';
+import { graphql } from 'gatsby';
+import { get } from 'lodash';
 
-const Blah = (props) => {
-  console.log('props123', props);
-  return (
-    <>
-
-    <Box as="h3">Blah page</Box>
-    <Box>testing testing</Box>
-    </>
-  );
-};
-
-const IndexPage = () => {
+const IndexPage = ({serverData, ...props}) => {
+  const {contractor, urlSlug} = serverData || {};
+  console.log('props', props);
   return (
     <Box p={[3]}>
       <Box as="h4">Root</Box>
-      <Router>
-        <Blah path="/:urlSlug?" />
-      </Router>
+      <p>urlSlug: {urlSlug}</p>
+      <Box as="p">{contractor?.businessName}</Box>
     </Box>
   )
 }
 
-export default IndexPage
+export default IndexPage;
 
-export const Head = () => {
-  const {title, description, twitterUsername} = useSiteMetadata();
+export const Head = ({serverData, ...props}) => {
+  const {contractor} = serverData || {};
+  const {description, twitterUsername} = useSiteMetadata();
+  const title = contractor ? `RenoFi and ${contractor.businessName}` : 'RenoFi - Home Renovation Financing';
 
   return (
     <>
@@ -42,4 +36,67 @@ export const Head = () => {
       <link rel="canonical" href="https://www.renofi.com/pros/test/blah" />
     </>
   );
+}
+
+const QUERY_CONTRACTOR_LOGO_BY_SLUG = `
+  query getContractorBySlug($urlSlug: String!) {
+    contractorBySlug(urlSlug: $urlSlug) {
+      businessName
+      logoUrl
+      daasPrice
+    }
+  }
+`;
+
+function gqlQuery (query) {
+  return async ({path, variables} = {}) => {
+    let req;
+    try {
+      req = await fetch(process.env.GATSBY_GRAPHQL_PROXY_URL, {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json",
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify({query: query, variables}),
+      });
+      const rsp = await req.json();
+      return path ? get(rsp, path) : rsp;
+    } catch (err) {
+      return err;
+    }
+  }
+};
+
+export async function getServerData({query, ...props}) {
+  const urlSlug = query?.contractor;
+  if (!urlSlug) {
+    return {
+      status: 200,
+      props: {},
+    };
+
+  }
+  try {
+    const path = ['data', 'contractorBySlug'];
+    const contractor = await gqlQuery(QUERY_CONTRACTOR_LOGO_BY_SLUG)({path, variables: {urlSlug}})
+    console.log('contractor', contractor);
+
+    // if (!res.ok) {
+    //   throw new Error(`Response failed`)
+    // }
+
+    return {
+      props: {
+        contractor,
+        urlSlug,
+      },
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      headers: {},
+      props: {}
+    }
+  }
 }
